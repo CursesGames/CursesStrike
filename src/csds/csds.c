b0e149446129059ba0e38f1a0838e3f16ef3d58b
Alexey Suslov <alexey.s+git@ultibot.ru>
2018-08-03 02:48:35 +0700

    Returned commit 668e0ba604247ef0e8e34f45b138b620cdd3324f by Sl1vo4ka

--- src/csds/csds.c
+++ src/csds/csds.c
@@ -331,6 +331,15 @@ int main(int argc, char **argv) {
             __syscall(sendto(u_fd, &(clients[id].public_info.position), sizeof(POINT), 0, (struct sockaddr *) &addr_client, addr_size));
             printf("coordinates were sent to client");
             delete_client(clients, addr_client);
+            
+            // Str1ker, 03.08.2018: вернул на место коммит 668e0ba604247ef0e8e34f45b138b620cdd3324f
+            // Receive message-CONNECT2
+            __syscall(result = recvfrom(u_fd, &cl_msg, sizeof(BCSMSG), 0, (struct sockaddr*) &addr_client, &addr_size));
+            printf("received from client: %.*s\n", result, msg);
+             // Change client stat if the previous was BCSCLST_CONNECTING
+            if(clients[id].public_info.state == BCSCLST_CONNECTING){
+                clients[id].public_info.state = BCSCLST_CONNECTED;
+            }
         }
     }
 
